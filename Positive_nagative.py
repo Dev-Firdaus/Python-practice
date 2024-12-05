4463796255ea3d2545e1f5095879bbf940bbd3c6
Md Firdaus <mdfirdaus.work05@gmail.com>
2024-12-05 19:09:39 +0530

    Number is nagative or positive

--- Positive_nagative.py
+++ Positive_nagative.py
@@ -1,8 +1,9 @@
-#Q3- Check if Number is Positive, Negative, or Zero...
-number=int(input("Enter a number"))
-if number > 0:
+# Check if Number is Positive, Negative, or Zero...
+number=int(input("Enter a number")) #take input from user
+
+if number > 0: # Check number is >0
     print("The number is positive")
-elif number < 0:
+elif number < 0: # Check number is < 0
     print("The number is nagative")
 else:
     print("The number is 0")

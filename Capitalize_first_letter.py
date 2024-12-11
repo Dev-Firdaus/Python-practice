882724d6e0ec51a5a27e71f8a9fd19bf07f9c9d6
Md Firdaus <mdfirdaus.work05@gmail.com>
2024-12-11 14:04:37 +0530

    Capitalize_first_lettr

--- Capitalize_first_letter.py
+++ Capitalize_first_letter.py
@@ -1,18 +1,18 @@
 
  # Capitalize first letter in a string
 
-def capitalize_words(input_string):
+def capitalize_words(input_string):  #take a function and pase a parameter
     # Use the title() method to capitalize the first letter of each word
-    capitalized_string = input_string.title()
-    return capitalized_string
+    capitalized_string = input_string.title() 
+    return capitalized_string #call that function... stored in var capitalized_string
 
 # Input string
 user_input = input("Enter a string: ")
 
 # Capitalize the first letter ....
-result = capitalize_words(user_input)
+result = capitalize_words(user_input) # fun.. capitalized_words is called with user_input
 
-# Output
+# Print Output
 print("Capitalized string:", result)
 
 
